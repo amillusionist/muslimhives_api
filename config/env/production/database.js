ab1b643110a9267d7298fe183b635c0ecf941ec6
Manish <manishwebworks@gmail.com>
2024-03-06 18:00:30 +0530

    node version manager

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'mysql',
       connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', '145.14.152.236'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME', 'u878230863_muslimhives'),
+        user: env('DATABASE_USERNAME', 'u878230863_muslimhives'),
+        password: env('DATABASE_PASSWORD', 'Dj@ANONMOUS007'),
         ssl: {
           cs: env('DATABASE_CA')
         },
